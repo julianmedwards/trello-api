9f47bcf0a06654ba927c94e3357ac6ed87ccbd38
julianmedwards <julian.michael.edwards@gmail.com>
2023-02-28 15:00:37 -0600

    Fixed mock data structure.

--- routes/lanes.js
+++ routes/lanes.js
@@ -1,7 +1,7 @@
 function getLanes(req, res, next) {
     const laneData = {
         laneName: 'lane 1',
-        cards: {cardName: 'card 1', cardDescr: 'card 2'},
+        cards: [{cardName: 'card 1', cardDescr: 'card 2'}],
     }
 
     console.log(laneData)
